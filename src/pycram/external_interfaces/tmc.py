7ca48c7d64730fbdfd74c34004810be4bf740316
makruse <makruse@uni-bremen.de>
2025-12-05 10:36:42 +0100

    tmc state

--- src/pycram/external_interfaces/tmc.py
+++ src/pycram/external_interfaces/tmc.py
@@ -1,58 +1,75 @@
-# from typing_extensions import Optional
-#
-# from ..datastructures.enums import GripperState
-# from ..robot_plans.motions import MoveGripperMotion
-# from ..ros import  loginfo
-# from ..ros import  create_publisher
-# from ..ros import  Rate
-#
-# is_init = False
-#
-#
-# def init_tmc_interface():
-#     global is_init
-#     if is_init:
-#         return
-#     from tmc_control_msgs.msg import GripperApplyEffortActionGoal
-#     from tmc_msgs.msg import Voice
-#     is_init = True
-#     loginfo("Successfully initialized tmc interface")
-#
-#
-#
-# def tmc_gripper_control(designator: MoveGripperMotion, topic_name: Optional[str] = '/hsrb/gripper_controller/grasp/goal'):
-#     """
-#     Publishes a message to the gripper controller to open or close the gripper for the HSR.
-#
-#     :param designator: The designator containing the motion to be executed
-#     :param topic_name: The topic name to publish the message to
-#     """
-#     if (designator.motion == GripperState.OPEN):
-#         pub_gripper = create_publisher(topic_name, GripperApplyEffortActionGoal, 10)
-#         rate = Rate(10)
-#         msg = GripperApplyEffortActionGoal()
-#         msg.goal.effort = 0.8
-#         pub_gripper.publish(msg)
-#
-#     elif (designator.motion == GripperState.CLOSE):
-#         pub_gripper = create_publisher(topic_name, GripperApplyEffortActionGoal, 10)
-#         rate = Rate(10)
-#         msg = GripperApplyEffortActionGoal()
-#         msg.goal.effort = -0.8
-#         pub_gripper.publish(msg)
-#
-#
-# def tmc_talk(designator: TalkingMotion, topic_name: Optional[str] = '/talk_request'):
-#     """
-#     Publishes a sentence to the talk_request topic of the HSRB robot
-#
-#     :param designator: The designator containing the sentence to be spoken
-#     :param topic_name: The topic name to publish the sentence to
-#     """
-#     pub = create_publisher(topic_name, Voice, 10)
-#     texttospeech = Voice()
-#     # language 1 = english (0 = japanese)
-#     texttospeech.language = 1
-#     texttospeech.sentence = designator.cmd
-#
-#     pub.publish(texttospeech)
+from typing_extensions import Optional
+from typing import Callable
+
+# from rclpy.qos import QoSProfile, ReliabilityPolicy
+import rclpy
+
+from tmc_voice_msgs.msg import Voice
+from ..ros.ros2.publisher import create_publisher
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+import rclpy
+from tmc_voice_msgs.msg import Voice
+from ..ros.ros2.publisher import create_publisher
+from rclpy.qos import QoSProfile, ReliabilityPolicy
+
+class TextToSpeechPublisher:
+    def __init__(self, node_name="tts", topic_name="/talk_request"):
+        self.node = rclpy.create_node(node_name)
+        qos = QoSProfile(depth=2, reliability=ReliabilityPolicy.RELIABLE)
+        self.pub = create_publisher(topic_name, Voice, self.node, qos)
+
+    def say(self, content: str):
+        msg = Voice()
+        msg.language = 1
+        msg.sentence = content or ""
+        self.pub.publish(msg)
+
+
+class TextToSpeechPublisher:
+
+    # Surprise its' initializations
+    is_init = False
+    rclpy.init()
+    tts_node = rclpy.create_node("tts")
+    # qos = QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE), would replace queue size
+    tts_pub = create_publisher("/talk_request", Voice, tts_node, 2)
+
+    @staticmethod
+    def init_talk_interface(func: Callable) -> Callable:
+        """Ensures initialization of the navigation interface before function execution."""
+
+        def wrapper(*args, **kwargs):
+            # Check if the interface is already initialized
+            if TextToSpeechPublisher.is_init:
+                return func(*args, **kwargs)
+            try:
+                from tmc_voice_msgs.msg import Voice        # This import is only needed if you intend to use voice
+            except ImportError:
+                logger.warning("Failed to import tmc_voice_msgs - package may not be installed")
+                return None
+            TextToSpeechPublisher.is_init = True
+            logger.info("Successfully initialized tmc interface")
+
+            return func(*args, **kwargs)
+        return wrapper
+
+    @init_talk_interface
+    def say(self, content : Optional[str] = ""):
+        """
+
+        :param content: The message to be spoken
+        :return:
+        """
+
+        talk_pub = TextToSpeechPublisher.tts_pub
+        msg = Voice()
+
+        msg.language = 1
+        msg.sentence = content
+
+        talk_pub.publish(msg)
+        return
